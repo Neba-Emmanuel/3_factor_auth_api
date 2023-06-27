e9ebdeb2dcddde203cbd6a6b473f7b13db2f1172
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-06-27 13:09:13 +0100

    updated

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Emmis:Emmisart@atlascluster.kccbvua.mongodb.net/?retryWrites=true&w=majority', {
+    await mongoose.connect('mongodb+srv://<username>:<pwd>@atlascluster.kccbvua.mongodb.net/?retryWrites=true&w=majority', {
     });
     console.log('MongoDB connected');
   } catch (error) {
